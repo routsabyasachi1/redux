fcc3f51bef0271abbacd8137ac05fdb96d5c1962
Sabyasachi Rout <sabyasachirout@byecom.in>
2023-06-29 14:03:11 +0530

    style update

--- src/pages/Dates.tsx
+++ src/pages/Dates.tsx
@@ -13,7 +13,9 @@ const Dates: React.FC = () => {
     setCurrentDate(currentDate.clone().add(1, "week"));
   };
 
-  const handleTimezoneChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleTimezoneChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     setSelectedTimezone(event.target.value);
   };
 
@@ -22,46 +24,76 @@ const Dates: React.FC = () => {
     const endDate = currentDate.clone().endOf("week");
     const days = [];
     const timeFormat = "hA";
-
+  
     for (let day = startDate.clone(); day.isSameOrBefore(endDate); day.add(1, "day")) {
       const formattedDay = day.format("dddd");
+      const formattedDate = day.format("MMMM Do");
+  
       const times = [];
-
+  
       for (let hour = 8; hour <= 23; hour++) {
-        const formattedTime = day.clone().hour(hour).tz(selectedTimezone).format(timeFormat);
+        const formattedTime = day
+          .clone()
+          .hour(hour)
+          .tz(selectedTimezone)
+          .format(timeFormat);
         times.push(
           <div key={`${formattedDay}-${hour}`} className="time-slot">
+            &nbsp;&nbsp;
             <input type="checkbox" id={`${formattedDay}-${hour}`} />
+            &nbsp;&nbsp;
             <label htmlFor={`${formattedDay}-${hour}`}>{formattedTime}</label>
+            &nbsp;&nbsp;
           </div>
         );
       }
-
+  
       days.push(
         <div key={formattedDay} className="day">
           <h3>{formattedDay}</h3>
-          {times}
+          <h4>{formattedDate}</h4>
+          <div className="flex flex-wrap w-screen">{times}</div>
+          ______________________________________________________________
         </div>
       );
     }
-
-    return days;
+  
+    return <div className="flex flex-col">{days}</div>;
   };
+  
 
-  const formattedDateTime = currentDate.tz(selectedTimezone).format("MMMM Do, YYYY, h:mmA");
+  const formattedDateTime = currentDate
+    .tz(selectedTimezone)
+    .format("MMMM Do, YYYY, h:mmA");
 
   return (
-    <div>
-      <h2>{formattedDateTime}</h2>
-      <div className="swipe-buttons">
-        <button onClick={handlePreviousWeek}>Previous</button>
-        <button onClick={handleNextWeek}>Next</button>
+    <div className="p-4">
+      <div className="flex space-x-4 mb-4">
+        <button
+          className="px-4 py-2 bg-blue-500 text-white rounded"
+          onClick={handlePreviousWeek}
+        >
+          Previous
+        </button>
+        <h2 className="text-2xl font-bold mb-4">{formattedDateTime}</h2>
+        <button
+          className="px-4 py-2 bg-blue-500 text-white rounded"
+          onClick={handleNextWeek}
+        >
+          Next
+        </button>
       </div>
-      <select value={selectedTimezone} onChange={handleTimezoneChange}>
+      <select
+        value={selectedTimezone}
+        onChange={handleTimezoneChange}
+        className="px-4 py-2 border border-gray-300 rounded"
+      >
         <option value="UTC">UTC</option>
         <option value="America/New_York">America/New_York</option>
       </select>
-      <div className="weekly-schedule">{renderWeeklySchedule()}</div>
+      <div className="grid grid-cols-7 gap-4 mt-4">
+        {renderWeeklySchedule()}
+      </div>
     </div>
   );
 };
